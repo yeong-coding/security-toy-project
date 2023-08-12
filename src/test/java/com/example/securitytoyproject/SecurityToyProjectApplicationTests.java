73b329bb27f86aebec1331a6153796f9770a5532
yeong-coding <qoskdud0928@gmail.com>
2023-08-12 17:18:30 +0900

    Initial commit

---
+++ src/test/java/com/example/securitytoyproject/SecurityToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitytoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
